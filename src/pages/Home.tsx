1d8a482ff2f0d708fb254b53d82090029b4c334f
William Roger <williamroger.frontend@gmail.com>
2022-07-19 15:05:37 -0300

    adicionada funcionadade para remover skills

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,6 +30,12 @@ export function Home() {
     setMySkills(oldState => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ));
+  }
+
   useEffect(() => {
     const currentHour = new Date().getHours();
     if (currentHour < 12) {
@@ -66,7 +72,10 @@ export function Home() {
         data={mySkills}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item.name} />
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
         )}
       />
     </View>
